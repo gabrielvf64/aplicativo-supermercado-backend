e4219adfbb3d678b558d77a86d7b54b95813f72c
gabrielvf64 <gabrielvf64@gmail.com>
2019-08-19 22:33:38 -0300

    [015] - Criados objetos e lista na inicialização para teste

--- src/main/java/com/gabrielferreira/aplicativo/AplicativoApplication.java
+++ src/main/java/com/gabrielferreira/aplicativo/AplicativoApplication.java
@@ -1,13 +1,32 @@
 package com.gabrielferreira.aplicativo;
 
+import com.gabrielferreira.aplicativo.dominio.Categoria;
+import com.gabrielferreira.aplicativo.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class AplicativoApplication {
+public class AplicativoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AplicativoApplication.class, args);
-	}
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(AplicativoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        // Criando objetos
+        Categoria categoria1 = new Categoria(null, "Bebidas");
+        Categoria categoria2 = new Categoria(null, "Açougue");
+
+        // Salvando em uma lista
+        categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+    }
 }
