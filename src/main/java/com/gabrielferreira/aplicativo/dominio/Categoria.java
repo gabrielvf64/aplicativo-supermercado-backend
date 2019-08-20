18b916da14e633a3cb1d91424d7497c21d8fcaaa
gabrielvf64 <gabrielvf64@gmail.com>
2019-08-19 21:11:28 -0300

    [013] - Dependencias do jpa, h2 e devtools; ORM de Categoria; Configurações de banco e mostrar queries no console

--- src/main/java/com/gabrielferreira/aplicativo/dominio/Categoria.java
+++ src/main/java/com/gabrielferreira/aplicativo/dominio/Categoria.java
@@ -3,14 +3,21 @@ package com.gabrielferreira.aplicativo.dominio;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Getter
 @Setter
+@Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
