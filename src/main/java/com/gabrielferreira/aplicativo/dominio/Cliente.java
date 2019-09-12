11d2dba241e543fb87bcd302f25cf1c298cac91b
gabrielvf64 <gabrielvf64@gmail.com>
2019-09-12 13:14:30 -0300

    [041] Deleta endereços com respectivo cliente deletado

--- src/main/java/com/gabrielferreira/aplicativo/dominio/Cliente.java
+++ src/main/java/com/gabrielferreira/aplicativo/dominio/Cliente.java
@@ -19,7 +19,7 @@ public class Cliente implements Serializable {
     private String cpfOuCnpj;
     private Integer tipoCliente;
 
-    @OneToMany(mappedBy = "cliente")
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
     @ElementCollection
