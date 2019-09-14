80b4e8123e6ba05fe132a58940e31ad7affb1401
gabrielvf64 <gabrielvf64@gmail.com>
2019-09-14 14:21:38 -0300

    [044] - Validação na atualização do Cliente

--- src/main/java/com/gabrielferreira/aplicativo/dto/ClienteDTO.java
+++ src/main/java/com/gabrielferreira/aplicativo/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.gabrielferreira.aplicativo.dto;
 
 import com.gabrielferreira.aplicativo.dominio.Cliente;
+import com.gabrielferreira.aplicativo.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
