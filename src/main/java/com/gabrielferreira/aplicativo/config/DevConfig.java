ffadec1da9e39f53f51225ec14980b46d7d6a5ca
gabrielvf64 <gabrielvf64@gmail.com>
2019-09-16 00:06:28 -0300

    [054] Criado profile de desenvolvimento e adicionando a dependência do mysql no pom.xml

---
+++ src/main/java/com/gabrielferreira/aplicativo/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.gabrielferreira.aplicativo.config;
+
+import com.gabrielferreira.aplicativo.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instanciarBancoDeTeste() throws ParseException {
+
+        if (!"create".equals(strategy)) {
+            return false;
+        }
+
+        dbService.instanciarBancoDeTeste();
+        return true;
+    }
+}
