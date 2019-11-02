3e45feb051dc2cb698f2918874d4c95bb10950d8
gabrielvf64 <gabrielvf64@gmail.com>
2019-11-02 17:51:41 -0300

    Criado arquivo de configuração para ambiente de produção

---
+++ src/main/java/com/gabrielferreira/aplicativo/config/ProdConfig.java
@@ -0,0 +1,39 @@
+package com.gabrielferreira.aplicativo.config;
+
+import com.gabrielferreira.aplicativo.services.DBService;
+import com.gabrielferreira.aplicativo.services.EmailService;
+import com.gabrielferreira.aplicativo.services.SmtpEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instanciarBancoDeTeste() throws ParseException {
+
+        if (!"create".equals(strategy)) {
+            return false;
+        }
+
+        dbService.instanciarBancoDeTeste();
+        return true;
+    }
+
+    @Bean
+    public EmailService emailService() {
+        return new SmtpEmailService();
+    }
+}
